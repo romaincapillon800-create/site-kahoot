2fdaede02d3d829fa2087e1643aeeee9421f408b
romaincapillon800-create <romaincapillon800@gmail.com>
2026-08-12 23:36:48 +0200

    feat: add restart game button to return to game config after finish

---
+++ src/lib/socket-server.ts
@@ -0,0 +1,3 @@
+// 🔒 SECURITY: This file re-exports the secure socket server implementation
+// The actual implementation is in socket-server-secure.ts
+export { initSocketServer } from "./socket-server-secure";
